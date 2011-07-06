cf460341a164d0d21dacf18c717285fa9b91a037
sugardrunk <sugardrunk@hear.fi>
2011-07-06 18:21:44 +0300

    small fix

--- notenal.py
+++ notenal.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 
 ## Notenal v.0.1.5
 
-##  Simple command-line notetaking & to-do list application
+##  Simple command-line notetaking & to-do list application.
 ##  Copyright (C) 2011 sugardrunk <http://sugardrunk.devio.us>
 
 
