8d6e328be8bf887f738def500bf641c930ea826a
sugardrunk <sugardrunk@hear.fi>
2012-01-28 17:00:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 
-## Notenal v.0.1.5
+## Notenal v.0.1.6
 
-##  Simple command-line notetaking & to-do list application
+##  Simple command-line notetaking application
 ##  Copyright (C) 2011 sugardrunk <http://sugardrunk.devio.us>
 
 
-import getpass, sys
+import getpass, sys, os
 
-print
-password = getpass.getpass ('SET PASSWORD: ')
-encoded_password = password.encode('rot13')
-print ('\n')('ENCODED PASSWORD:'), (encoded_password), ('\n''\n')
+password = getpass.getpass ('\n''SET PASSWORD: ')
+encoded_password = password.encode('rot13') 
+
+# write to file
+if os.path.exists('./setup/'):
+    filename = ('setup')      	
+    file = open ('./setup/' + filename, 'w')                   
+    file.write (encoded_password)            
+    file.close()
+else:
+    os.makedirs('./setup/')
+    filename = ('setup')		
+    file = open ('./setup/' + filename, 'w')                       
+    file.write (encoded_password)            
+    file.close()
+
+print '\n''Password set!', '\n''\n'
+
 sys.exit(0)
