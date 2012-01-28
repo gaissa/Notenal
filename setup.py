e0fc3d73179a1b0853e7e6be85c2fc29e8873f79
sugardrunk <sugardrunk@hear.fi>
2012-01-28 19:18:10 +0200

    version 0.1.7

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
 # -*- coding: utf-8 -*-
 
 
-## Notenal v.0.1.6
+## Notenal v.0.1.7
 
 ##  Simple command-line notetaking application
-##  Copyright (C) 2011 sugardrunk <http://sugardrunk.devio.us>
+##  Copyright (C) 2012 sugardrunk <http://sugardrunk.devio.us>
 
 
 import getpass, sys, os
 
+# get password
 password = getpass.getpass ('\n''SET PASSWORD: ')
+
+# encode password
 encoded_password = password.encode('rot13') 
 
-# write to file
+# write encoded password to file
 if os.path.exists('./setup/'):
-    filename = ('setup')      	
-    file = open ('./setup/' + filename, 'w')                   
-    file.write (encoded_password)            
+    filename = ('setup')
+    file = open ('./setup/' + filename, 'w')
+    file.write (encoded_password)
     file.close()
 else:
     os.makedirs('./setup/')
-    filename = ('setup')		
-    file = open ('./setup/' + filename, 'w')                       
-    file.write (encoded_password)            
+    filename = ('setup')
+    file = open ('./setup/' + filename, 'w')
+    file.write (encoded_password)
     file.close()
 
-print '\n''Password set!', '\n''\n'
-
+print '\n''Password set!', '\n'
 sys.exit(0)
