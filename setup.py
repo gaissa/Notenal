7fff79f35941a56b05455f4953007369ac556fac
sugardrunk <sugardrunk@hear.fi>
2012-08-28 23:02:22 +0300

    more like it should be (at least for a moment)

--- setup.py
+++ setup.py
@@ -1,32 +1,36 @@
 # -*- coding: utf-8 -*-
 
 
-## Notenal v.0.1.7
+## Notenal v.0.1.8
 
 ##  Simple command-line notetaking application
 ##  Copyright (C) 2012 sugardrunk <http://sugardrunk.devio.us>
 
 
-import getpass, sys, os
+import getpass
+import sys
+import os
 import base64
+
 # get password
-password = getpass.getpass ('\n''SET PASSWORD: ')
+password = getpass.getpass('\n''SET PASSWORD: ')
 
 # encode password
 encoded_password = base64.b64encode(password)
 
 # write encoded password to file
-if os.path.exists('./setup/'):
-    filename = ('setup')
-    file = open ('./setup/' + filename, 'w')
-    file.write (encoded_password)
+if os.path.exists('./notenal_ setup/'):
+    filename = ('notenal_setup')
+    file = open('./notenal_setup/' + filename, 'w')
+    file.write(encoded_password)
     file.close()
 else:
-    os.makedirs('./setup/')
-    filename = ('setup')
-    file = open ('./setup/' + filename, 'w')
-    file.write (encoded_password)
+    os.makedirs('./notenal_setup/')
+    filename = ('notenal_setup')
+    file = open('./notenal_setup/' + filename, 'w')
+    file.write(encoded_password)
     file.close()
 
+# ...
 print '\n''Password set!', '\n'
 sys.exit(0)
