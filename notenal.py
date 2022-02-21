f768d1df000c0941101a0e9c4e9a44b90796792b
gaissa <sugardrunk@hear.fi>
2022-02-21 07:32:03 +0200

    Annual 10 year update :D

--- notenal.py
+++ notenal.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
 
-## Notenal v.0.1.8
+## Notenal v.0.1.9
 
 ##  A Simple command-line notetaking application
-##  Copyright (C) 2012 sugardrunk <http://koti.tamk.fi/~c1jkahko/>
+##  Copyright (C) 2012-2022 gaissa <https://github.com/gaissa>
 
 
 import base64
@@ -15,120 +15,137 @@ import os
 import sys
 import time
 
-# setup
-if os.path.exists('./notenal_setup/'):
-    setup = open('./notenal_setup/' + 'notenal_setup')
-    password = setup.read()
-else:
-    os.makedirs('./notenal_setup/')
-    setup = open('./notenal_setup/' + 'notenal_setup', 'w')
-    setup.write('')
-    setup = open('./notenal_setup/' + 'notenal_setup')
-    password = setup.read()
+def main():
+    # set time
+    now = time.localtime()
 
-# set time
-now = time.localtime()
+    # set max attempt(s)
+    attempt = 1
+    max_attempts = 3
 
-# set max attempt(s)
-attempt = 1
-max_attempts = 3
+    # set separator line
+    separator = '-' * 48
 
-# set separator line
-separator = '-' * 48
+    # print title
+    title = 'NOTENAL v.0.1.9'
+    print '\n\n', title
+    print '=' * len(title) + '\n'
 
-# print title
-title = 'NOTENAL v.0.1.8'
-print '\n''\n', title
-print '=' * len(title), '\n'
+    # ask password
+    while (attempt <= max_attempts):
+        pw = getpass.getpass('Password: ')
+        if base64.b64encode(pw) == (password):
+            print '\nPassword correct! Welcome to Notenal!'
+            break
+        else:
+            print '\nIncorrect password, ' \
+            'you have', (max_attempts - attempt), 'attempt(s) left\n\n'
+        attempt = attempt + 1
 
-# ask password
-while (attempt < max_attempts + 1):
-    pw = getpass.getpass('Password: ')
-    if base64.b64encode(pw) == (password):
-        print '\n''Password correct!''\n''\n'
-        break
-    else:
-        print '\n''Wrong password, ' \
-        'You have', (max_attempts - attempt), 'attempt(s) left'
-    attempt = attempt + 1
+    # quit after max attempts
+    if (attempt > max_attempts):
+        print 'You have exceeded ' \
+        'the maximum number of attempts!\n\nNotenal closing...\n\n'
+        sys.exit(0)
 
-# quit after max attempts
-if (attempt > max_attempts):
-    print '\n\nYou have exceeded ' \
-    'the maximum number of attempts!\n\nNotenal closing...\n'
-    sys.exit(0)
-
-# run menu
-while True:
-
-    menu = raw_input('[R]ead, [W]rite, [L]ist files or [Q]uit?: ')
-
-    # read
-    if menu == "R":
-
+    def readNotes():
         # read file
-        readfile = raw_input('\n''FILE NAME: ')
-        print '\n' + separator + '\n'
-        print (readfile), 'CONTENTS:'
+        readfile = raw_input('\nFILE NAME: ')
         try:
             file = open('./notenal_notes/' + readfile)
+            print '\n' + separator + '\n'
+            print (readfile), 'CONTENTS:'
             print file.read()
-            print '\n' + separator
+            print separator
             file.close()
         except:
-            print '\n''\n' + separator
-            print 'File not found!''\n''\n'
+            print '\nFile does not exist!'
 
-    # write
-    if menu == "W":
-
+    def writeNotes():
         # set file name
-        filename = raw_input('\n''FILE NAME: ')
-        note = raw_input('\n''YOUR NOTE: ')
-        print ('\n''OUTPUT TO'), (filename) + ':', (note)
-        under1 = (datetime.datetime.now().ctime())
+        filename = raw_input('\nFILE NAME: ')
 
-        # write to file
-        if os.path.exists('./notenal_notes/'):
-            file = open('./notenal_notes/' + filename, 'a')
-            file.write('\n''\n' + datetime.datetime.now().ctime() + '\n')
-            file.write('=' * len(under1))
-            file.write('\n' + note + '\n')
-            file.close()
+        if filename is '':
+            print '\nFilename cannot be empty! Set a name for your note!'
         else:
-            os.makedirs('./notenal_notes/')
-            file = open('./notenal_notes/' + filename, 'a')
-            file.write('\n''\n' + datetime.datetime.now().ctime() + '\n')
-            file.write('=' * len(under1))
-            file.write('\n' + note + '\n')
-            file.close()
+            # get note and timestamp
+            note = raw_input('\nYOUR NOTE: ')
+            time = (datetime.datetime.now().ctime())
 
-        # read file
-        print '\n' + separator + '\n'
-        print (filename), 'CONTENTS:'
-        file = open('./notenal_notes/' + filename)
-        print file.read()
-        file.close()
+            # write to file
+            if os.path.exists('./notenal_notes/'):
+                file = open('./notenal_notes/' + filename, 'a')
+                file.write('\n\n' + time + '\n')
+                file.write('=' * len(time))
+                file.write('\n' + note + '\n')
+                file.close()
+            else:
+                os.makedirs('./notenal_notes/')
+                file = open('./notenal_notes/' + filename, 'a')
+                file.write('\n\n' + time + '\n')
+                file.write('=' * len(time))
+                file.write('\n' + note + '\n')
+                file.close()
 
-        # print character count
-        print '\n' + separator + '\n'
-        print 'Your note was', len(note), 'character(s) in length... \n''\n'
+                # read file
+                print '\n' + separator + '\n'
+                print (filename), 'CONTENTS:'
+                file = open('./notenal_notes/' + filename)
+                print file.read()
+                file.close()
 
-    # list files
-    if menu == "L":
+                # print character count
+                print '\n' + separator + '\n'
+                print 'Your note was', len(note), 'character(s) in length...'
 
-        try:
+    def listNotes():
+        if len(os.listdir('./notenal_notes/')) != 0:
             print '\n' + separator
             for list_files in os.listdir('./notenal_notes/'):
                 print ('\n' + list_files)
                 print '=' * len(list_files)
-            print '\n''\n' + separator + '\n'
-        except:
-            if not os.path.exists('./notenal_notes/'):
-                print '\n''No files found!''\n'
-                print '\n' + separator + '\n'
+            print '\n' + separator
+        else:
+            print '\nNo files found!'
 
-    # quit
-    if menu == "Q":
-        print '\n''Notenal closing...''\n'
+    def deleteNotes():
+        filename = raw_input('\nFILE NAME: ')
+        try:
+            os.remove('./notenal_notes/' + filename)
+            print('\nFile ' + filename + ' deleted!')
+        except:
+            print '\nFile does not exist!'
+
+    def quitNotenal():
+        print '\nNotenal closing...\n\n'
         sys.exit(0)
+
+    # run menu
+    while True:
+        menu = raw_input('\n\n\033[1m[R]\033[0mead \033[96m-\033[0m ' \
+                             '\033[1m[W]\033[0mrite \033[96m-\033[0m ' \
+                             '\033[1m[L]\033[0mist \033[96m-\033[0m ' \
+                             '\033[1m[D]\033[0melete \033[96m-\033[0m ' \
+                             '\033[1m[Q]\033[0muit: ')
+        if menu == "R":
+            readNotes()
+
+        if menu == "W":
+            writeNotes()
+
+        if menu == "L":
+            listNotes()
+
+        if menu == "D":
+            deleteNotes()
+
+        if menu == "Q":
+            quitNotenal()
+
+# get password and run the app
+if os.path.exists('./notenal_setup/'):
+    setup = open('./notenal_setup/' + 'notenal_setup')
+    password = setup.read()
+    main()
+else:
+    print '\nNo password set! Run setup.py to set one!\n'
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
 
-## Notenal v.0.1.8
+## Notenal v.0.1.9
 
 ##  A simple command-line notetaking application
-##  Copyright (C) 2012 sugardrunk <http://koti.tamk.fi/~c1jkahko/>
+##  Copyright (C) 2012-2022 sugardrunk <https://github.com/gaissa>
 
 
 import getpass
@@ -12,25 +13,29 @@ import sys
 import os
 import base64
 
+SETUP_FOLDER = './notenal_setup/'
+SETUP_FILE = 'notenal_setup'
+CHECK_FOLDER = os.path.isdir(SETUP_FOLDER)
+
 # get password
-password = getpass.getpass('\n''SET PASSWORD: ')
+password = getpass.getpass('\nSET PASSWORD: ')
 
 # encode password
 encoded_password = base64.b64encode(password)
 
-# write encoded password to file
-if os.path.exists('./notenal_ setup/'):
-    filename = ('notenal_setup')
-    file = open('./notenal_setup/' + filename, 'w')
-    file.write(encoded_password)
-    file.close()
-else:
-    os.makedirs('./notenal_setup/')
-    filename = ('notenal_setup')
-    file = open('./notenal_setup/' + filename, 'w')
+# file operations helper
+def helper():
+    file = open(SETUP_FOLDER + SETUP_FILE, 'w')
     file.write(encoded_password)
     file.close()
 
-# ...
-print '\n''Password set!', '\n'
+# write encoded password to file
+if not CHECK_FOLDER:
+    os.makedirs(SETUP_FOLDER)
+    helper()
+    print '\nPassword set!\n'
+else:
+    helper()
+    print '\nPassword changed!\n'
+
 sys.exit(0)
